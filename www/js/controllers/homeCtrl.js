4197d168e1133dd46cea3ca3b9f945195e43eeaa
Higor Diego <higordiegoti@gmail.com>
2018-04-23 21:22:09 -0300

    subindo

--- www/js/controllers/homeCtrl.js
+++ www/js/controllers/homeCtrl.js
@@ -50,6 +50,22 @@
 					$cordovaBluetoothSerial.connect(address).then(function () {
 						$cordovaBluetoothSerial.isConnected().then(function () {
 							$ionicLoading.hide();
+							var recep = 'teste de impressora \n\n'
+							$cordovaBluetoothSerial.write(recep).then(function (a, b) {
+								$ionicLoading.hide();
+								$state.go('call')
+							}, function (err) {
+								$ionicLoading.hide();
+								var alertPopup = $ionicPopup.alert({
+									title: 'Error',
+									template: 'Não conseguimos encontrar a Impressora'
+								});
+								alertPopup.then(function () {
+									$ionicLoading.hide();
+									$state.go('home')
+								});
+
+							});
 							confirmPrint()
 						}, function (err) {
 							$ionicLoading.hide();
